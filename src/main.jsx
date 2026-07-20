06ff0f61b28e11cece2b93c45e3a150a94426db8
isaac0226 <37863094+isaac0226@users.noreply.github.com>
2026-07-20 18:07:53 +0900

    Load high-contrast quiz feedback styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import QuizHub from './QuizHub';
 import './styles.css';
 import './quiz-extra.css';
+import './quiz-feedback.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
