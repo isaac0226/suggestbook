e8d021f3354ea6bad6aaefc2f957acc1c1482753
isaac0226 <37863094+isaac0226@users.noreply.github.com>
2026-07-25 19:58:01 +0900

    Load title-first book input styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import QuizHub from './QuizHub';
 import './styles.css';
 import './quiz-extra.css';
 import './quiz-feedback.css';
+import './quiz-book-input.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
